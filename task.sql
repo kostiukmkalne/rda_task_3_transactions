4e19e473414ca94aaba3c843e7c5ff2982a464c4
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-04-13 13:52:39 -0500

    new solution

--- task.sql
+++ task.sql
@@ -1,7 +1,14 @@
 USE ShopDB;
 
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2024-04-13');
+
 START TRANSACTION;
-INSERT INTO Orders (CustomerID, Date) VALUES (1, '2024-04-12');
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
-INSERT INTO OrderItems (OrderID, ProductID, Count) VALUES (LAST_INSERT_ID(), 1, 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (1, 1, 1);
+
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1 WHERE Products.ID = 1;
+
 COMMIT;
